cf782e82bba4573b6004c90fff500fe7432abc9f
Chih-Sheng (Tommy) Huang <chih.sheng.huang821@gmail.com>
2017-03-31 14:59:27 +0800

    Update DNN_Test.m

--- DeepNeuralNetwork/DNN_Test.m
+++ DeepNeuralNetwork/DNN_Test.m
@@ -3,5 +3,5 @@ Normal_testdata=testdata;
 for i=1:size(testdata,2)
     Normal_testdata(:,i)= (testdata(:,i)-DNN_net.mu_train(i,1)) /DNN_net.sigma_trian(i,1);
 end
-DNN_test=forwordpropagation(Normal_testdata',DNN_net);
+DNN_test=DNN_feedbackward(Normal_testdata',DNN_net);
 predictresult=DNN_test.V{end};
